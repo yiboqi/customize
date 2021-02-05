152a7b2dbe8c45c71f667c8ce6b754808db03db6
yiboqi <2371823759@qq.com>
2021-02-05 18:25:48 +0800

    清理不必要文件

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <Home/>
 </template>
 
 <script>
+import Home from './views/Home/Home'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Home
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "assets/css/normalize.css";
 </style>
